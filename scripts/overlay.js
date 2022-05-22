e4daff5587d77d48442acb8e5e2a003aa1a375da
Vaent <gh@vaent.uk>
2022-05-22 16:02:05 +0100

    Add URI fragment handling
    Individual images can be identified in the URI fragment to load them
    automatically (e.g. /gallery.html#image-1 or /gallery.html#image-1.png)
    to facilitate bookmarking and sharing a specific image.

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -11,3 +11,22 @@ function expandImage(thumb) {
 function collapseImage() {
     expandedImageOverlay.style.display = "none";
 }
+
+function handleUriFragment() {
+    if (location.hash) {
+        let frag = location.hash.substring(1);
+        let img = document.querySelector(`img[src*="${frag}." i], img[src$="${frag}" i]`);
+        if (img) {
+            expandImage(img);
+            return;
+        } else {
+            location.hash = "";
+        }
+    }
+    collapseImage();
+}
+
+addEventListener('hashchange', handleUriFragment);
+
+// check and action any initially supplied fragment
+handleUriFragment();

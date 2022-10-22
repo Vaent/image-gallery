c42328dfbaa9bffaca067929ac6f078d081dbe95
Vaent <gh@vaent.uk>
2022-10-22 16:20:58 +0100

    Tidy ImageGallery code

--- java-image-gallery/src/main/java/uk/vaent/java_image_gallery/ImageGallery.java
+++ java-image-gallery/src/main/java/uk/vaent/java_image_gallery/ImageGallery.java
@@ -1,16 +1,14 @@
 package uk.vaent.java_image_gallery;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.vaent.java_image_gallery.model.DatabaseAccess;
@@ -21,27 +19,34 @@ import uk.vaent.java_image_gallery.model.ImageAndFileDetails;
 @RequestMapping("/gallery")
 public class ImageGallery {
 
+    private final ClassPathResource staticResourcesDirectory = new ClassPathResource("static/");
+
     @Autowired
     DatabaseAccess databaseAccess;
 
     @GetMapping
     public String gallery(ModelMap model) {
         try {
-            List<Image> imageDetails = databaseAccess.getAllBasicImageDetails();
-            List<ImageAndFileDetails> imageAndFileDetailsList = new ArrayList<>();
-            for (Image image : imageDetails) {
-                String filename = image.imageName().toLowerCase().replace(" ", "-");
-                String filePath = "images/" + filename + "." + image.fileFormat();
-                if (new ClassPathResource("static/" + filePath).exists()) {
-                    imageAndFileDetailsList.add(new ImageAndFileDetails(image, filename, filePath));
-                }
-            }
+            List<ImageAndFileDetails> imageAndFileDetailsList = databaseAccess.getAllBasicImageDetails()
+                .stream()
+                .map(this::checkImageFile)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
             model.addAttribute("imageFileList", imageAndFileDetailsList);
         } catch (SQLException ex) {
             System.out.println(ex);
             model.addAttribute("imageFileList", new ArrayList<ImageAndFileDetails>());
         }
         return "gallery";
+    }
+
+    private ImageAndFileDetails checkImageFile(Image image) {
+        String filename = image.imageName().toLowerCase().replace(" ", "-");
+        String filePath = "images/" + filename + "." + image.fileFormat();
+        if (staticResourcesDirectory.createRelative(filePath).exists()) {
+            return new ImageAndFileDetails(image, filename, filePath);
+        }
+        return null;
     }
 
 }
